2b61a92d62701bb2a39b3825bb6f2dd152c2190d
Yerman <germanwibaux@gmail.com>
2018-09-19 22:10:08 -0300

    Add property working

--- src/app/components/new-property/new-property.component.ts
+++ src/app/components/new-property/new-property.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PropiedadInterface } from '../../models/propiedadInterface';
+import { NgForm } from '@angular/forms';
+import { PropertyService } from '../../services/property.service';
 
 @Component({
   selector: 'app-new-property',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPropertyComponent implements OnInit {
 
-  constructor() { }
+  property : PropiedadInterface = {
+    banios:'',
+    cocheras:'',
+    nombre: '',
+    codigo: '',
+    estado: '',
+    descripcion:'',
+    geolocalizacion: '',
+    habitaciones: '',
+    localidad: '',
+    operacion: '',
+    precio: '',
+    propiedad: '',
+    superficieCubierta: '',
+    superficieTotal: ''
+  }
+
+  constructor(private propertyService: PropertyService) { }
 
   ngOnInit() {
+  }
+
+  onSaveProperty(formInput: NgForm){
+    console.log(this.property);
+    this.propertyService.addProperty(this.property);
   }
 
 }
--- src/app/services/property.service.ts
+++ src/app/services/property.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { PropiedadInterface } from '../models/propertyInterface';
+import { PropiedadInterface } from '../models/propiedadInterface';
 import { Observable } from 'rxjs';
+import { Action } from 'rxjs/internal/scheduler/Action';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -13,12 +15,34 @@ export class PropertyService {
   propertyDoc: AngularFirestoreDocument <PropiedadInterface>;
 
   constructor( public afs: AngularFirestore ) { 
-    this.properties = afs.collection('propiedades').valueChanges();    
+    //this.properties = afs.collection('propiedades').valueChanges();
+    this.propertyCollection = afs.collection<PropiedadInterface>('properties');
+    this.properties = this.propertyCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as PropiedadInterface;
+        const id = a.payload.doc.id;
+        return { id, ...data};
+      }))
+    );    
    }
 
 
    getProperties() {
      return this.properties;
    }
+
+   addProperty(property: PropiedadInterface){
+     this.propertyCollection.add(property);
+     
+   }
+
+   deletProperty() {
+    console.log('Delete property');
+   }
+
+   updateProperty() {
+     console.log('Update property');
+   }
+
 
 }
