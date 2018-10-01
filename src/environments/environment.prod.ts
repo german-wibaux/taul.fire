957db7c69b66017f2859a889bb5b962a9674539a
Yerman <germanwibaux@gmail.com>
2018-10-01 13:21:42 -0300

    Deploy and site general site working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyACNTywab_0gY84yzrFBzgCy_GP6YslhY4",
+    authDomain: "taul-fire.firebaseapp.com",
+    databaseURL: "https://taul-fire.firebaseio.com",
+    projectId: "taul-fire",
+    storageBucket: "taul-fire.appspot.com",
+    messagingSenderId: "184740146240"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
     apiKey: "AIzaSyACNTywab_0gY84yzrFBzgCy_GP6YslhY4",
     authDomain: "taul-fire.firebaseapp.com",
