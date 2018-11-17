80d0f275615c8ac8fdf336814a770a3d4238b39d
Yerman <germanwibaux@gmail.com>
2018-11-17 12:40:46 -0300

    Reparacion de actualizacion de imagenes

--- src/app/components/update-property/update-property.component.ts
+++ src/app/components/update-property/update-property.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { PropertyService } from 'src/app/services/property.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PropiedadInterface } from 'src/app/models/propiedadInterface';
+import { Observable, from } from 'rxjs';
+import { AngularFireUploadTask, AngularFireStorage } from 'angularfire2/storage';
+import { switchMap, tap, finalize } from 'rxjs/operators';
 // import { Route } from '@angular/compiler/src/core';
 
 @Component({
@@ -11,9 +14,15 @@ import { PropiedadInterface } from 'src/app/models/propiedadInterface';
 })
 export class UpdatePropertyComponent implements OnInit {
   property: PropiedadInterface = null;
+  uploadPercent: Observable<number>;
+  downloadURL: Observable<string>; // For download files
+  snapshot: Observable<any>;
+  // Main task
+  task: AngularFireUploadTask;
   constructor(private propertyService: PropertyService , 
               private route: ActivatedRoute, 
-              private router:Router) { }
+              private router:Router,
+              private storage: AngularFireStorage) { }
 
   ngOnInit() { 
     // this.propertyService.getProperty()
@@ -37,6 +46,43 @@ export class UpdatePropertyComponent implements OnInit {
       this.router.navigate(['/edit']);
    
    }
+
+   uploadFile(event) {
+    // console.log(event);
+
+    const file = event.target.files[0];
+
+
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('unsupported file type taul-app')
+      return;
+    }
+    let loading = true;
+    const day = new Date();
+    const path = 'properties/' + day.getTime() + '/' + file.name;
+    const customMetadata = { app: 'taul-app' };
+    const ref = this.storage.ref(path);
+    this.task = this.storage.upload(path, file, { customMetadata });
+    this.uploadPercent = this.task.percentageChanges();
+
+
+    return from(this.task).pipe(
+      switchMap(() => ref.getDownloadURL()),
+      tap(url => {
+        // use url here, e.g. assign it to a model       
+        this.property.imagen.push(url);        
+      }),
+      finalize(() => loading = false)
+    ).subscribe(() => {
+      // success
+    }, error => {
+      // failure
+      console.log(error);
+    });
+
+    //*********************Codigo pa la barra de progreso y algo mas si pinta************************
+    //     <div>{{ uploadPercent | async }}</div>
+  }
 
 
 }
