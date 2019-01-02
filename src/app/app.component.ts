b026c8911d77f4245eb06b5db7be88e2c213765e
Yerman <germanwibaux@gmail.com>
2019-01-02 16:13:56 -0300

    Deploy angular cloud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+
+    if (environment.production) {
+       if (location.protocol === 'http:') {
+        window.location.href = location.href.replace('http', 'https');
+       }
+      }    
+     }
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   firebaseConfig : {
     apiKey: "AIzaSyACNTywab_0gY84yzrFBzgCy_GP6YslhY4",
-    authDomain: "taul-fire.firebaseapp.com",
+    authDomain: "taulinmobiliaria.com.ar" ,
     databaseURL: "https://taul-fire.firebaseio.com",
     projectId: "taul-fire",
     storageBucket: "taul-fire.appspot.com",
